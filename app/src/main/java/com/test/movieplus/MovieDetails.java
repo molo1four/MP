5a26a159d12d4266e28689cc350c3a3ea8805efc
molo1four <adnee@naver.com>
2020-10-05 17:47:12 +0900

    1

---
+++ app/src/main/java/com/test/movieplus/MovieDetails.java
@@ -0,0 +1,14 @@
+package com.test.movieplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MovieDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+    }
+}
