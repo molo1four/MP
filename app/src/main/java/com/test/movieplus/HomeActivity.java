a5d71a16846770cf52626e1dffd3bc0cea2c7c89
molo1four <adnee@naver.com>
2020-10-08 16:40:51 +0900

    1

--- app/src/main/java/com/test/movieplus/HomeActivity.java
+++ app/src/main/java/com/test/movieplus/HomeActivity.java
@@ -1,13 +1,12 @@
 package com.test.movieplus;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -18,11 +17,16 @@ public class HomeActivity extends AppCompatActivity {
     FirstFragment firstFragment;
     SecondFragment secondFragment;
     ThirdFragment thirdFragment;
+    MenuItem mitem_search;
+    MenuItem mitem_reset;
+    MenuItem mitem_logout;
+    MenuItem mitem_withdrawal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);      firstFragment = new FirstFragment();
+        setContentView(R.layout.activity_home);
+        firstFragment = new FirstFragment();
         secondFragment = new SecondFragment();
         thirdFragment = new ThirdFragment();
 
@@ -68,6 +72,40 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_overflow, menu);
+        mitem_search = menu.findItem(R.id.mitem_search);
+        mitem_reset = menu.findItem(R.id.mitem_reset);
+        mitem_logout = menu.findItem(R.id.mitem_logout);
+        mitem_withdrawal = menu.findItem(R.id.mitem_withdrawal);
+
+//        mitem_search.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+//            @Override
+//            public boolean onMenuItemClick(MenuItem item) {
+//                Intent i = new Intent(getApplicationContext(),SearchActivity.class);
+//                startActivity(i);
+//                return true;
+//            }
+//        });
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.mitem_search){
+            Intent i = new Intent(this,SearchActivity.class);
+            startActivity(i);
+        }
+        if(id == R.id.mitem_reset){
+            Intent i = new Intent(this,SearchActivity.class);
+            startActivity(i);
+        }
+        if(id == R.id.mitem_logout){
+            return true;
+        }
+        if(id == R.id.mitem_withdrawal){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
